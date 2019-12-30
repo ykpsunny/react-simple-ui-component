d6afbfe17a4d90d8de723ddd459dde2e8e8a8508
Sunny <2309456411@qq.com>
2019-12-30 23:44:12 +0800

    fix: popover arrow position

--- src/components/Popover/Popover.js
+++ src/components/Popover/Popover.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 
 import "./Popover.scss";
 
@@ -7,25 +7,35 @@ import propTypes from "prop-types";
 import classnames from "classnames";
 
 function Popover({ content, children, className, position }) {
-	const clas = classnames('simple-popover-content', `simple-popover-content-${position}`)
+	const clas = classnames(
+		"simple-popover-wrapper",
+		`simple-popover-wrapper-${position}`
+	);
 	return (
-		<div className="simple-popover-wrapper">
+		<Fragment>
 			<div className={clas}>
-				<div className="simple-popover-inner">{content}</div>
+				<div className="simple-popover-content">{content}</div>
+				<div className="simple-popover-arrow"></div>
 			</div>
 			<div className="simple-popover-children">{children}</div>
-		</div>
+		</Fragment>
 	);
 }
 
 Popover.defaultProps = {
-	position: 'topCenter'
+	position: "topCenter"
 };
 
 Popover.propTypes = {
 	content: propTypes.node, // 显示内容
-	position: propTypes.oneOf(['leftCenter', 'topCenter', 'bottomCenter', 'rightCenter']), // 显示位置
-	className: propTypes.string // 容器类名
+	position: propTypes.oneOf([
+		"leftCenter",
+		"topCenter",
+		"bottomCenter",
+		"rightCenter"
+	]), // 显示位置
+	className: propTypes.string, // 容器类名
+	trigger: propTypes.oneOf(['hover', 'click', 'contextMenu'])
 };
 
 export default Popover;

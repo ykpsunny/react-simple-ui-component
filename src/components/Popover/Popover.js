58fab33b0184f8ca34f5c351c0e9045fae9faf4f
Sunny <2309456411@qq.com>
2020-01-01 23:19:50 +0800

    add popover content position

--- src/components/Popover/Popover.js
+++ src/components/Popover/Popover.js
@@ -1,4 +1,9 @@
-import React, { useEffect, useState, Fragment } from "react";
+import React, {
+	useEffect,
+	useState,
+	Fragment,
+	useRef
+} from "react";
 
 import "./Popover.scss";
 
@@ -6,24 +11,123 @@ import propTypes from "prop-types";
 
 import classnames from "classnames";
 
-function Popover({ content, children, className, position }) {
+import ReactDOM from "react-dom";
+
+function Popover({
+	content,
+	children,
+	className,
+	position,
+	visible: defaultVisible,
+	trigger
+}) {
+	const [visible, setVisible] = useState(defaultVisible);
 	const clas = classnames(
 		"simple-popover-wrapper",
 		`simple-popover-wrapper-${position}`
 	);
+
+	const childrenRef = useRef();
+	const contentRef = useRef();
+
+	useEffect(() => {
+		setPosition();
+	});
+
+	function setPosition() {
+		if (!visible) {
+			return;
+		}
+		let childrenNode = childrenRef.current,
+			contentNode = contentRef.current,
+			childrenNodeOffset = childrenNode.getBoundingClientRect(),
+			contentNodeOffset = contentNode.getBoundingClientRect(),
+			transformOrigin = {
+				left: "50%",
+				top: "50%"
+			};
+		if (position.includes("top")) {
+			transformOrigin.top = `${childrenNodeOffset.top -
+				contentNodeOffset.height -
+				5}px`;
+			transformOrigin.left = `${childrenNodeOffset.left -
+				contentNodeOffset.width / 2 +
+				childrenNodeOffset.width / 2}px`;
+		} else if (position.includes("left")) {
+			transformOrigin.top = `${childrenNodeOffset.top -
+				childrenNodeOffset.height / 2}px`;
+			transformOrigin.left = `${childrenNodeOffset.left -
+				contentNodeOffset.width -
+				5}px`;
+		} else if (position.includes("right")) {
+			transformOrigin.top = `${childrenNodeOffset.top -
+				contentNodeOffset.height / 2}px`;
+			transformOrigin.left = `${childrenNodeOffset.left +
+				childrenNodeOffset.width +
+				15}px`;
+		} else if (position.includes("bottom")) {
+			transformOrigin.top = `${childrenNodeOffset.top +
+				contentNodeOffset.height -
+				15}px`;
+			transformOrigin.left = `${childrenNodeOffset.left -
+				contentNodeOffset.width / 2 +
+				childrenNodeOffset.width / 2}px`;
+		}
+		contentNode.style.left = transformOrigin.left;
+		contentNode.style.top = transformOrigin.top;
+	}
+
+	function renderContent() {
+		return ReactDOM.createPortal(
+			<div className={clas} ref={contentRef} onClick={e => e.stopPropagation()}>
+				<div className={classnames("simple-popover-content", className)}>
+					{content}
+				</div>
+				<div className="simple-popover-arrow"></div>
+			</div>,
+			document.body
+		);
+	}
+
+	function clickHandle (e) {
+		e.stopPropagation();
+		show()
+	}
+
+	function show () {
+		setVisible(true);
+		window.addEventListener('click', hide, false)
+	}
+
+	function hide () {
+		setVisible(false)
+		window.removeEventListener('click', hide, false)
+	}
+
+	useEffect(() => {
+		document.addEventListener("mousewheel", mouseWheelHandle, false);
+		return () => {
+			document.addEventListener('mousewheel', mouseWheelHandle, false)
+		}
+	}) 
+
+	function mouseWheelHandle () {
+		setPosition()
+	}
+
 	return (
 		<Fragment>
-			<div className={clas}>
-				<div className="simple-popover-content">{content}</div>
-				<div className="simple-popover-arrow"></div>
+			{visible && renderContent()}
+			<div className="simple-popover-children" ref={childrenRef} onClick={clickHandle}>
+				{children}
 			</div>
-			<div className="simple-popover-children">{children}</div>
 		</Fragment>
 	);
 }
 
 Popover.defaultProps = {
-	position: "topCenter"
+	position: "bottomCenter",
+	visible: true
 };
 
 Popover.propTypes = {
@@ -34,8 +138,9 @@ Popover.propTypes = {
 		"bottomCenter",
 		"rightCenter"
 	]), // 显示位置
-	className: propTypes.string, // 容器类名
-	trigger: propTypes.oneOf(['hover', 'click', 'contextMenu'])
+	className: propTypes.string, // 内容类名
+	trigger: propTypes.oneOf(["hover", "click", "contextMenu"]), // 触发事件
+	visible: propTypes.bool // 是否显示气泡
 };
 
 export default Popover;
